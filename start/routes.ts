b2146f12dcc0af2a87f01859e6fcde844c388758
Jac Wang <lu7766lu7766@gmail.com>
2026-08-04 15:40:39 +0800

    router group, auth, api_format

--- start/routes.ts
+++ start/routes.ts
@@ -14,12 +14,21 @@ import UsersController from '#controllers/users_controller'
 router.on('/').renderInertia('home', {}).as('home')
 router.on('/demo').renderInertia('demo', {}).as('demo')
 
-router.post('api/login', [UsersController, 'login'])
-router.get('api/self', [UsersController, 'self']) //.use(middleware.auth())
-router.post('api/logout', [UsersController, 'logout'])
+router
+  .group(() => {
+    router.get('', () => 'ok')
+    router.post('login', [UsersController, 'login'])
+    router
+      .group(() => {
+        router.get('self', [UsersController, 'self']) //.use(middleware.auth())
+        router.post('logout', [UsersController, 'logout'])
+      })
+      .use(middleware.auth())
+  })
+  .prefix('api')
+  .use(middleware.api_format())
 // Restful API
 // GET 查詢
 // POST 新增
 // PUT 更新
 // DELETE 刪除
-router.post('api', () => 'ok')
